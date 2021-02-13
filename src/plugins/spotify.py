9b8967efb59ca498bc68c2d5e393e38515ae613b
Samarth Jugran <jugransamarth@gmail.com>
2021-02-13 19:23:16 +0530

    add spotify auto start

--- src/plugins/spotify.py
+++ src/plugins/spotify.py
@@ -1,4 +1,5 @@
 import subprocess
+from time import sleep
 from src import config
 
 
@@ -7,9 +8,13 @@ def switch_to_light():
     if spotify_theme:
         subprocess.run(['spicetify', 'config', 'current_theme', 'Dribbblish', 'color_scheme', spotify_theme]) # Shell theme
         subprocess.run(['spicetify', 'apply'])
+        sleep(2.5)
+        subprocess.run(['spotify'])
 
 def switch_to_dark():
     spotify_theme = config.get("spotifyDarkTheme")
     if spotify_theme:
         subprocess.run(['spicetify', 'config', 'current_theme', 'Dribbblish', 'color_scheme', spotify_theme]) # Shell theme
         subprocess.run(['spicetify', 'apply'])
+        sleep(2.5)
+        subprocess.run(['spotify'])
